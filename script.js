2327a6b38b45f3ac55c5703a1d0606937dae91c1
msz4 <misbahu.s.z@gmail.com>
2019-05-24 11:05:23 +0100

    JS functions created the required divs for grid display by checking screen width
    TODO: store svg images, lload images into divs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(screen.width)
+(function (){
+    let n = 0;
+    let divs = "";
+    if(screen.width > 768){
+        n = 6;
+    }
+    else{
+        n = 4;
+    }
+
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            divs+="<div>"+i+j+"</div>"
+        }
+    }
+    console.log(divs);
+    document.querySelector("#main").innerHTML = divs;
+})();
